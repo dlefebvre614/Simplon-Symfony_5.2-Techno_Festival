acd9547af2c394282ef01c2fa0b8076317cefa8c
dlefebvre614 <didier.lefebvre12@gmail.com>
2021-06-21 13:53:09 +0200

    Add homepage

---
+++ src/Controller/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    /**
+     * @Route("/", name="page")
+     */
+    public function home(): Response
+    {
+        return $this->render('page/home.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+}
