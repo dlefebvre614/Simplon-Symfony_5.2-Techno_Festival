cdb6639e084039b429636f50487b3aae1e2e34f8
dlefebvre614 <didier.lefebvre12@gmail.com>
2021-06-23 11:08:57 +0200

    modify artist

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -15,16 +15,47 @@ class ArtistController extends AbstractController
     {
         $border_color = ["secondary", "danger", "info", "warning", "light", "success"];
         $bg_color = ["secondary", "danger", "info", "warning"];
+        /*$categories = [
+            "Mélodique" => [
+                "color" => "danger",
+                "name" => "Mélodique",
+            ],
+            'Industrielle' => [
+                "color" =>  'Info',
+                "name" => "Industrielle",
+            ],
+            'Groovy' => [
+                "color" => 'secondary',
+                "name" => "Groovy",
+            ],
+            'Deep' => [
+                "color" =>  'success',
+                "name" => "Deep",
+            ],
+            'Détroit' => [
+                "color" =>  'warning',
+                "name" => "Détroit",
+            ],
+        ];*/
+        $categories = [
+            'Mélodique' => 'danger',
+            'Industrielle' => 'info',
+            'Groovy' => 'secondary',
+            'Deep' => 'warning',
+            'Détroit' => 'success',
+        ];
+        // recupérer les catégories du controleur category
 
         return $this->render('artist/index.html.twig', [
             'controller_name' => 'ArtistController',
             'border_colors' => $border_color,
             'bg_colors' => $bg_color,
+            'categories' => $categories,
         ]);
     }
 
     /**
-     * @Route("/artist/{id<\d+})", name="show_artist")
+     * @Route("/artist/{id<\d+>}", name="show_artist")
      */
     public function show(int $id): Response
     {
